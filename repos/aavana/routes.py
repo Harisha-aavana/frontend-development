b29804034310cf037f00348b09f30f53b8be1116
Harisha-aavana <62471934+Harisha-aavana@users.noreply.github.com>
2023-11-10 23:03:29 +0530

    comment id added in getCommentHistory

--- repos/aavana/routes.py
+++ repos/aavana/routes.py
@@ -8,7 +8,6 @@ import os
 from repos import db
 from repos.users.routes import logout
 
-
 aavana = Blueprint('aavana', __name__)
 
 
@@ -69,7 +68,7 @@ def aavana_home():
 
         return jsonify(response)
 
-    return render_template('aavana_home_v2.html')
+    return render_template('aavana_home.html')
 
 
 @aavana.route('/upload', methods=['POST'])
@@ -99,7 +98,7 @@ def upload_file():
             file_path = os.path.join(task_dir, filename)
             document_data['file_path'] = file_path[5:]
             file.save(file_path)
-            
+
             document_info = Document(file_name=filename, task_id=task_id)
             db.session.add(document_info)
             db.session.flush()
@@ -108,7 +107,8 @@ def upload_file():
             document_data['saved_document_id'] = saved_document_id
             document_data['message'] = "File uploaded successfully"
             message = f'Document inserted - doc id {saved_document_id}'
-            log_info = Log(action=message, table_name='document', user_id=current_user.id, action_time=datetime.datetime.now())
+            log_info = Log(action=message, table_name='document', user_id=current_user.id,
+                           action_time=datetime.datetime.now())
             db.session.add(log_info)
             db.session.commit()
             db.session.close()
@@ -129,7 +129,8 @@ def delete_document():
         document_to_update.file_status = 1
 
         message = f'Document deleted - doc id {document_to_update.id}'
-        log_info = Log(action=message, table_name='document', user_id=current_user.id, action_time=datetime.datetime.now())
+        log_info = Log(action=message, table_name='document', user_id=current_user.id,
+                       action_time=datetime.datetime.now())
         db.session.add(log_info)
         # Commit the changes to the database
         db.session.commit()
@@ -163,15 +164,44 @@ def add_comment():
         type = request.form.get('comment_type')
         current_date = datetime.datetime.now()
         try:
-            message = f'Comment added to task {task_id}'
-            comments = Comments(comment=description, comment_type=type, task_id=task_id, created_date=current_date, modified_date='', comment_status=0)
+            message = f'Comment added to task id {task_id}'
+            comments = Comments(comment=description, comment_type=type, task_id=task_id, created_date=current_date,
+                                modified_date='', comment_status=0)
             log_info = Log(action=message, table_name='comment', user_id=current_user.id,
                            action_time=current_date)
             db.session.add(comments)
             db.session.add(log_info)
             db.session.commit()
             return {"message": "Comment Added Successfully."}
-        except:
+        except Exception as e:
+            # Handle exceptions or errors here
+            db.session.rollback()
+            return "An error occurred: " + str(e)
+
+    return {'message': 'Invalid request method'}
+
+
+@aavana.route('/edit_comment', methods=['POST', 'GET'])
+@login_required
+def edit_comment():
+    if request.method == 'POST':
+        comment_id = request.form.get('id')
+        task_id = request.form.get('task_id')
+        updated_comment = request.form.get('comment')
+        type = request.form.get('comment_type')
+        modified_date = datetime.datetime.now()
+        try:
+            comment_update = Comments.query.get(comment_id)
+            comment_update.comment = updated_comment
+            comment_update.comment_type = type
+            comment_update.modified_date = modified_date
+            message = f'Comment edited. task id {task_id}'
+            log_info = Log(action=message, table_name='comment', user_id=current_user.id,
+                           action_time=modified_date)
+            db.session.add(log_info)
+            db.session.commit()
+            return {"message": "Comment Added Successfully."}
+        except Exception as e:
             # Handle exceptions or errors here
             db.session.rollback()
             return "An error occurred: " + str(e)
@@ -184,17 +214,18 @@ def add_comment():
 def getCommentsHistory():
     if request.method == 'POST':
         task_id = request.form.get('task_id')
-        print(task_id)
         try:
-            comments_data = Comments.query.order_by(Comments.created_date.desc()).all()
+            comments_data = Comments.query.filter(Comments.task_id == task_id).order_by(Comments.created_date.desc()).all()
             data = []
             for comment in comments_data:
+                comment_id = comment.comment_id
                 comment_details = comment.comment
                 commented_date = comment.created_date.strftime('%d/%m/%Y')
                 type = comment.comment_type
-                comment = [commented_date, comment_details, type]
+                comment = [comment_id, commented_date, comment_details, type]
                 data.append(comment)
-            return data
+
+            return jsonify(data)
 
         except Exception as e:
             return "An error occurred: " + str(e)
